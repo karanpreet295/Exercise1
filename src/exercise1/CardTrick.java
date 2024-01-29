8f211164d3f9b7b2f613cb1a2cd60d71b0e841fe
karanpreet kaur gill <gill1017@sheridancollege.ca>
2024-01-29 16:58:39 -0500

    Final changes for exercise1

--- src/exercise1/CardTrick.java
+++ src/exercise1/CardTrick.java
@@ -58,7 +58,7 @@ public class CardTrick {
      * @author Paul Bonenfant Jan 2022
      */
     private static void printInfo() {
-    
+    /*"I'm done!"**/
         System.out.println("Congratulations, you guessed right!");
         System.out.println();
         
